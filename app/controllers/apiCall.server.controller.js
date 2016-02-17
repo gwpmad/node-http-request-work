249c5b99e06044ccb9cb263d4dc3b4eca037ce8e
gwpmad <georgemaddocks@yahoo.co.uk>
2016-02-17 10:26:31 +0000

    request works, callback implemented

--- app/controllers/apiCall.server.controller.js
+++ app/controllers/apiCall.server.controller.js
@@ -1,55 +1,14 @@
 var request = require('request');
 
-function getBody(url, callback) {
-  request({
-    url: url,
-    json: true,
-  }, function(error, response, body) {
-    if (error || response.statusCode !== 200) {
-      return callback(error || {statusCode: response.statusCode});
-    }
-    callback(null, JSON.parse(body));
+
+
+module.exports = function(callback){
+  request('https://openapi.etsy.com/v2/listings/active?api_key=96kg7j4073g5j4nc9tk5usgw', function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        callback(null, JSON.parse(body));
+
+      } else {
+        console.log(error);
+      }
   });
-}
-
-
-module.exports = function(req, res) {
-  var etsyData;
-  getBody('https://openapi.etsy.com/v2/listings/active?api_key=96kg7j4073g5j4nc9tk5usgw', function(err, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      etsyData = body;
-    }
-  });
-  console.log(etsyData);
-  return etsyData;
-
-
-
-
-
-
-
-
-  // var myReturn;
-  // request('https://openapi.etsy.com/v2/listings/active?api_key=96kg7j4073g5j4nc9tk5usgw', function(error, response, body) {
-  //     if (!error && response.statusCode === 200) {
-  //       var etsyData = JSON.parse(body);
-  //       myReturn = etsyData.results;
-  //     } else {
-  //       console.log(error);
-  //     }
-  //     return myReturn;
-  // });
-
-
-  // request('https://openapi.etsy.com/v2/listings/active?api_key=96kg7j4073g5j4nc9tk5usgw', function(error, response, body) {
-  //   if (!error && response.statusCode === 200) {
-  //     var etsyData = JSON.parse(body);
-  //     return(JSON.parse(etsyData.results));
-  //   } else {
-  //     console.log(error);
-  //   }
-  // });
 };
--- app/controllers/price.server.controller.js
+++ app/controllers/price.server.controller.js
@@ -1,34 +1,42 @@
 var apiCall = require('./apiCall.server.controller.js');
-var returnObject = {};
 
-exports.request = function(req,res){
-  var etsyData = apiCall(req,res);
-  returnObject.average_price = getAveragePrice(etsyData);
-  returnObject.highest_price = getHighestPricedListing(etsyData);
-  returnObject.lowest_price = getLowestPricedListing(etsyData);
-  res.json(returnObject);
+exports.request = function(req, res) {
+  var returnObject = {};
+  var etsyData;
+  apiCall(function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      etsyData = data.results;
+      returnObject.average_price = getAveragePrice(etsyData);
+      returnObject.highest_priced_listing = getHighestPricedListing(etsyData);
+      returnObject.lowest_priced_listing = getLowestPricedListing(etsyData);
+      res.json(returnObject);
+      console.log(returnObject);
+    }
+  });
 };
 
-getAveragePrice = function(json){
+getAveragePrice = function(json) {
   var total = 0;
-  for(i = 0; i < json.length; i++){
+  for (i = 0; i < json.length; i++) {
     total += parseInt(json[i].price);
   }
-  return total/json.length;
+  return total / json.length;
 };
 
-getHighestPricedListing = function(json){
+getHighestPricedListing = function(json) {
   sortArrayByPriceLowestToHighest(json);
   return json.pop();
 };
 
-getLowestPricedListing = function(json){
+getLowestPricedListing = function(json) {
   sortArrayByPriceLowestToHighest(json);
   return json[0];
 };
 
-sortArrayByPriceLowestToHighest = function(array){
-  array.sort(function (a, b) {
+sortArrayByPriceLowestToHighest = function(array) {
+  array.sort(function(a, b) {
     if (parseInt(a.price) > parseInt(b.price)) return 1;
     if (parseInt(a.price) < parseInt(b.price)) return -1;
     return 0;
